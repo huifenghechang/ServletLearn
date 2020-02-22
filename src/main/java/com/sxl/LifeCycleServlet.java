4e90a7e236ef351b9ad834f5aa364c486094a5c6
su-xiao-le <suqiancheng_su@163.com>
2020-02-22 22:04:01 +0800

    Initial commit

---
+++ src/main/java/com/sxl/LifeCycleServlet.java
@@ -0,0 +1,36 @@
+package com.sxl;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @Auther: 苏小乐
+ * @Date: 2020/2/22 21:56
+ * @Description:
+ */
+public class LifeCycleServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("Servlet init ... ");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet service ... ");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet destroy ... ");
+    }
+}
